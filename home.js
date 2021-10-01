5f7089dc1bbdf8c1e9934f5f674e4822457663c5
xfaraday <theonlyfaraday@gmail.com>
2021-10-01 09:54:45 -0700

    rdy 2 pty

--- home.js
+++ home.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+//const port = 80
 const port = 4343
 
 app.use(express.static(path.join(__dirname, 'site')))
@@ -10,26 +11,38 @@ app.get('/', function (req, res) {
 })
 
 app.get('/webpage', function (req, res) {
-	res.sendFile(path.join(__dirname,'site/index.html'))
+	res.send({"status":"past the page huh?"})
 })
 
 app.get('/webpage/site/', function (req, res) {
 	res.sendFile(path.join(__dirname,'site/index.html'))
 })
 
+app.get('/webpage/site/prod', function (req, res) {
+	res.send({"status":"cold"})
+})
+
 app.get('/admin', function (req, res) {
-	res.sendFile(path.join(__dirname,'site/index.html'))
+	res.send({"status":"past the page huh?"})
 })
 
 app.get('/admin/testdrive', function (req, res) {
 	res.sendFile(path.join(__dirname,'site/index.html'))
 })
 
-app.get('/admin/testdrive', function (req, res) {
-	res.sendFile(path.join(__dirname,'site/index.html'))
+app.get('/admin/testdrive/auth', function (req, res) {
+	res.send({"status":"warmer"})
+})
+
+app.get('/admin/testdrive/auth/restore', function (req, res) {
+	res.sendFile(path.join(__dirname,'site/flag.txt'))
 })
 
 app.get('/storage', function (req, res) {
+	res.send({"status":"past the page huh?"})
+})
+
+app.get('/nah', function (req, res) {
 	res.sendFile(path.join(__dirname,'site/index.html'))
 })
 
